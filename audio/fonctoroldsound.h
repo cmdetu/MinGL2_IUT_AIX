9bb55f6e6ee49060bfe2234f5d18d77c9259904e
cmdetu <clement.mathieu_drif@outlook.fr>
2020-09-19 09:14:28 +0200

    Doc for audio system

--- audio/fonctoroldsound.h
+++ audio/fonctoroldsound.h
@@ -6,12 +6,24 @@
 
 namespace nsAudio
 {
-
+/**
+ * @class FonctorOldSound
+ * @brief Fonctor de recherche des sons terminés
+ */
 class FonctorOldSound : public nsUtil::IFonctorUnaire<std::unique_ptr<sf::Music>, bool>
 {
 
 public:
+    /**
+     * @brief Définit le fichier audio de la musique
+     * @param[in] eff : effet audio à tester
+     * @fn virtual bool operator()(const std::unique_ptr<sf::Music>& eff) const override;
+     */
     virtual bool operator()(const std::unique_ptr<sf::Music>& eff) const override;
+    /**
+     * @brief Destructeur pour la classe FonctorOldSound
+     * @fn virtual ~FonctorOldSound() override = default;
+     */
     virtual ~FonctorOldSound() override = default;
 
 }; // class FonctorOldSound
