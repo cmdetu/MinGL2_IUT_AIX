cmdetu/MinGL2_IUT_AIX

commit 7eeed0352b6b0e5cf3dcd62bfb2f3b7c6bde8a8b

Added some getter/setter

include/mingl/shape/circle.h
@@ -32,7 +32,7 @@ public:
      * @param[in] borderColor : Couleur de bord
      * @fn Circle(const nsGraphics::Vec2D& position, const unsigned& radius, const nsGraphics::RGBAcolor& fillColor, const nsGraphics::RGBAcolor& borderColor = nsGraphics::KTransparent);
      */
-    Circle(const nsGraphics::Vec2D& position, const unsigned& radius, const nsGraphics::RGBAcolor& fillColor, const nsGraphics::RGBAcolor& borderColor = nsGraphics::KTransparent);
+    Circle(const nsGraphics::Vec2D& getPosition, const unsigned& getRadius, const nsGraphics::RGBAcolor& fillColor, const nsGraphics::RGBAcolor& borderColor = nsGraphics::KTransparent);
 
     /**
      * @brief Destructeur virtuel pour la classe Circle
@@ -47,7 +47,7 @@ public:
      * @param[in] position : Position a additionner
      * @fn Circle operator+(const nsGraphics::Vec2D& position) const
      */
-    Circle operator+(const nsGraphics::Vec2D& position) const;
+    Circle operator+(const nsGraphics::Vec2D& getPosition) const;
 
     /**
      * @brief Opérateur de réduction
@@ -55,6 +55,32 @@ public:
      * @fn Circle operator*(const float& f) const
      */
     Circle operator*(const float& f) const;
+
+    /**
+     * @brief Récupère la position du cercle
+     * @fn const nsGraphics::Vec2D& getPosition() const;
+     */
+    const nsGraphics::Vec2D& getPosition() const;
+
+    /**
+     * @brief Définit la nouvelle position du cercle
+     * @param[in] position : Nouvelle position
+     * @fn void setPosition(const nsGraphics::Vec2D &position);
+     */
+    void setPosition(const nsGraphics::Vec2D &position);
+
+    /**
+     * @brief Récupère le rayon du cercle
+     * @fn unsigned getRadius() const;
+     */
+    unsigned getRadius() const;
+
+    /**
+     * @brief Définit le nouveau rayon du cercle
+     * @param[in] radius : Nouveau rayon
+     * @fn void setRadius(const unsigned &radius);
+     */
+    void setRadius(const unsigned &radius);
 
 private:
     /**

examples/00-Boilerplate/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
     // Initialise le système
-    MinGL window("00 - Boilerplate", nsGraphics::Vec2D(640, 640), nsGraphics::KBlack);
+    MinGL window("00 - Boilerplate", nsGraphics::Vec2D(640, 640), nsGraphics::Vec2D(128, 128), nsGraphics::KBlack);
     window.initGlut();
     window.initGraphic();
 

examples/01-Shapes/main.cpp
@@ -40,7 +40,7 @@ void dessiner(MinGL &window)
 int main()
 {
     // Initialise le système
-    MinGL window("01 - Shapes", nsGraphics::Vec2D(640, 640), nsGraphics::KBlack);
+    MinGL window("01 - Shapes", nsGraphics::Vec2D(640, 640), nsGraphics::Vec2D(128, 128), nsGraphics::KBlack);
     window.initGlut();
     window.initGraphic();
 

examples/02-Texte/main.cpp
@@ -42,7 +42,7 @@ void dessiner(MinGL &window)
 int main()
 {
     // Initialise le système
-    MinGL window("02 - Texte", nsGraphics::Vec2D(640, 640), nsGraphics::KBlack);
+    MinGL window("02 - Texte", nsGraphics::Vec2D(640, 640), nsGraphics::Vec2D(128, 128), nsGraphics::KBlack);
     window.initGlut();
     window.initGraphic();
 

examples/03-Clavier/main.cpp
@@ -33,7 +33,7 @@ void dessiner(MinGL &window)
 int main()
 {
     // Initialise le système
-    MinGL window("03 - Clavier", nsGraphics::Vec2D(640, 640), nsGraphics::KBlack);
+    MinGL window("03 - Clavier", nsGraphics::Vec2D(640, 640), nsGraphics::Vec2D(128, 128), nsGraphics::KBlack);
     window.initGlut();
     window.initGraphic();
 

include/mingl/graphics/rgbacolor.h
@@ -45,7 +45,7 @@ public:
     /**
      * @brief Opérateur d'égalité
      * @param[in] col : Couleur a vérifier
-     * @fn Circle operator+(const nsGraphics::Vec2D& position) const
+     * @fn bool operator==(const RGBAcolor& col) const;
      */
     bool operator==(const RGBAcolor& col) const;
 

include/mingl/gui/text.h
@@ -97,14 +97,14 @@ public:
     /**
      * @brief Calcule la position visible du texte, calculée avec l'alignement vertical et horizontal
      * @return La position visible, en haut a gauche
-     * @fn Vec2D computeVisibleposition() const;
+     * @fn nsGraphics::Vec2D computeVisiblePosition() const;
      */
     nsGraphics::Vec2D computeVisiblePosition() const;
 
     /**
      * @brief Calcule la position de fin visible du texte, calculée avec l'alignement vertical et horizontal
      * @return La position visible, en bas a droite
-     * @fn Vec2D computeVisibleEndPosition() const;
+     * @fn nsGraphics::Vec2D computeVisibleEndPosition() const;
      */
     nsGraphics::Vec2D computeVisibleEndPosition() const;
 
@@ -121,6 +121,71 @@ public:
      * @fn void setContent(const std::string &content);
      */
     void setContent(const std::string &content);
+
+    /**
+     * @brief Récupère la position du texte
+     * @fn const nsGraphics::Vec2D& getPosition() const;
+     */
+    const nsGraphics::Vec2D& getPosition() const;
+
+    /**
+     * @brief Définit la nouvelle position du texte
+     * @param[in] position : Nouvelle position
+     * @fn void setPosition(const nsGraphics::Vec2D &position);
+     */
+    void setPosition(const nsGraphics::Vec2D &position);
+
+    /**
+     * @brief Récupère la couleur du texte
+     * @fn const nsGraphics::RGBAcolor& getTextColor() const;
+     */
+    const nsGraphics::RGBAcolor& getTextColor() const;
+
+    /**
+     * @brief Définit la nouvelle couleur du texte
+     * @param[in] textColor : Nouvelle couleur
+     * @fn void setTextColor(const nsGraphics::RGBAcolor &textColor);
+     */
+    void setTextColor(const nsGraphics::RGBAcolor &textColor);
+
+    /**
+     * @brief Récupère la police du texte
+     * @fn const GlutFont& getTextFont() const;
+     */
+    const GlutFont& getTextFont() const;
+
+    /**
+     * @brief Définit la nouvelle police du texte
+     * @param[in] textFont : Nouvelle police
+     * @fn void setTextFont(const GlutFont &textFont);
+     */
+    void setTextFont(const GlutFont &textFont);
+
+    /**
+     * @brief Récupère l'alignement horizontal du texte
+     * @fn HorizontalAlignment getHorizontalAlignment() const;
+     */
+    HorizontalAlignment getHorizontalAlignment() const;
+
+    /**
+     * @brief Définit le nouvel alignement horizontal du texte
+     * @param[in] horizontalAlignment : Nouvel alignement horizontal
+     * @fn void setHorizontalAlignment(const HorizontalAlignment &horizontalAlignment);
+     */
+    void setHorizontalAlignment(const HorizontalAlignment &horizontalAlignment);
+
+    /**
+     * @brief Récupère l'alignement vertical du texte
+     * @fn VerticalAlignment getVerticalAlignment() const;
+     */
+    VerticalAlignment getVerticalAlignment() const;
+
+    /**
+     * @brief Définit le nouvel alignement vertical du texte
+     * @param[in] verticalAlignment : Nouvel alignement vertical
+     * @fn void setVerticalAlignment(const VerticalAlignment &verticalAlignment);
+     */
+    void setVerticalAlignment(const VerticalAlignment &verticalAlignment);
 
 private:
     /**

include/mingl/mingl.h
@@ -58,10 +58,15 @@ public:
      * @brief Constructeur pour la classe MinGL
      * @param[in] name : Nom de la fenêtre
      * @param[in] windowSize : Taille de la fenêtre
+     * @param[in] windowPosition : Position de la fenêtre
      * @param[in] backgroundColor : Couleur de fond de la fenêtre
-     * @fn MinGL(const std::string& name, const nsGraphics::Vec2D& windowSize = nsGraphics::Vec2D(640, 480), const nsGraphics::RGBAcolor& backgroundColor = nsGraphics::KWhite);
+     * @fn MinGL(const std::string& name, const nsGraphics::Vec2D& windowSize = nsGraphics::Vec2D(640, 480),
+     *    const nsGraphics::Vec2D& windowPosition = nsGraphics::Vec2D(128, 128),
+     *    const nsGraphics::RGBAcolor& backgroundColor = nsGraphics::KWhite);
      */
-    MinGL(const std::string& name, const nsGraphics::Vec2D& windowSize = nsGraphics::Vec2D(640, 480), const nsGraphics::RGBAcolor& backgroundColor = nsGraphics::KWhite);
+    MinGL(const std::string& name, const nsGraphics::Vec2D& windowSize = nsGraphics::Vec2D(640, 480),
+          const nsGraphics::Vec2D& windowPosition = nsGraphics::Vec2D(128, 128),
+          const nsGraphics::RGBAcolor& backgroundColor = nsGraphics::KWhite);
 
     /**
      * @brief Destructeur de la classe MinGL
@@ -137,9 +142,42 @@ public:
 
     /**
      * @brief Récupère la taille de la fenêtre
-     * @fn const nsGraphics::Vec2D getWindowSize() const;
+     * @fn nsGraphics::Vec2D getWindowSize() const;
      */
-    const nsGraphics::Vec2D getWindowSize() const;
+    nsGraphics::Vec2D getWindowSize() const;
+
+    /**
+     * @brief Règle la taille de la fenêtre
+     * @param[in] windowSize : Nouvelle taille
+     * @fn void setWindowSize(const nsGraphics::Vec2D& windowSize);
+     */
+    void setWindowSize(const nsGraphics::Vec2D& windowSize);
+
+    /**
+     * @brief Récupère la position de la fenêtre
+     * @fn nsGraphics::Vec2D getWindowPosition() const;
+     */
+    nsGraphics::Vec2D getWindowPosition() const;
+
+    /**
+     * @brief Règle la position de la fenêtre
+     * @param[in] windowPosition : Nouvelle position
+     * @fn void setWindowPosition(const nsGraphics::Vec2D &windowPosition);
+     */
+    void setWindowPosition(const nsGraphics::Vec2D &windowPosition);
+
+    /**
+     * @brief Récupère le nom de la fenêtre
+     * @fn const std::string& getWindowName() const;
+     */
+    const std::string& getWindowName() const;
+
+    /**
+     * @brief Règle le nom de la fenêtre
+     * @param[in] windowName : Nouveau nom
+     * @fn void setWindowName(const std::string &windowName);
+     */
+    void setWindowName(const std::string &windowName);
 
     /**
      * @brief Retourne si la fenêtre est ouverte
@@ -151,39 +189,34 @@ private:
     // Les données membres en tant que tel
 
     /**
-     * @brief windowSize : Taille de la fenêtre
+     * @brief m_windowName : Nom de la fenêtre
      */
-    const nsGraphics::Vec2D windowSize;
+    std::string m_windowName;
 
     /**
-     * @brief windowName : Nom de la fenêtre
+     * @brief m_bgColor : Couleur de fond de la fenêtre
      */
-    const std::string windowName;
+    nsGraphics::RGBAcolor m_bgColor;
 
     /**
-     * @brief bgColor : Couleur de fond de la fenêtre
+     * @brief m_keyboardMap : Map de l'état des touches du clavier
      */
-    nsGraphics::RGBAcolor bgColor;
+    KeyMap_t m_keyboardMap;
 
     /**
-     * @brief keyboardMap : Map de l'état des touches du clavier
+     * @brief m_glutWindowId : Identifiant de la fenêtre freeglut
      */
-    KeyMap_t keyboardMap;
+    short m_glutWindowId = 0;
 
     /**
-     * @brief glutWindowId : Identifiant de la fenêtre freeglut
+     * @brief m_eventManager : Gestionnaire d'évènements
      */
-    short glutWindowId = 0;
+    nsEvent::EventManager m_eventManager;
 
     /**
-     * @brief eventManager : Gestionnaire d'évènements
+     * @brief m_windowIsOpen : Etat de la fenêtre
      */
-    nsEvent::EventManager eventManager;
-
-    /**
-     * @brief windowIsOpen : Etat de la fenêtre
-     */
-    bool windowIsOpen;
+    bool m_windowIsOpen;
 
     // Les handlers
 

commit 34ea91bf1622bf0dc33bb6545756fc4303d10b97

Fix some problems introduced with 7eeed03

include/mingl/shape/circle.h
@@ -46,7 +46,7 @@ public:
      * @param[in] borderColor : Couleur de bord
      * @fn Circle(const nsGraphics::Vec2D& position, const unsigned& radius, const nsGraphics::RGBAcolor& fillColor, const nsGraphics::RGBAcolor& borderColor = nsGraphics::KTransparent);
      */
-    Circle(const nsGraphics::Vec2D& getPosition, const unsigned& getRadius, const nsGraphics::RGBAcolor& fillColor, const nsGraphics::RGBAcolor& borderColor = nsGraphics::KTransparent);
+    Circle(const nsGraphics::Vec2D& position, const unsigned& radius, const nsGraphics::RGBAcolor& fillColor, const nsGraphics::RGBAcolor& borderColor = nsGraphics::KTransparent);
 
     /**
      * @brief Destructeur virtuel pour la classe Circle

include/mingl/shape/shape.h
@@ -52,9 +52,9 @@ public:
 
     /**
      * @brief Règle la couleur de remplissage
-     * @fn cvoid setFillColor(nsGraphics::RGBAcolor& fillColor);
+     * @fn cvoid setFillColor(const nsGraphics::RGBAcolor& fillColor);
      */
-    void setFillColor(nsGraphics::RGBAcolor& fillColor);
+    void setFillColor(const nsGraphics::RGBAcolor& fillColor);
 
     /**
      * @brief Retourne la couleur de bord
@@ -64,9 +64,9 @@ public:
 
     /**
      * @brief Règle la couleur de bord
-     * @fn void setBorderColor(RGBAcolor& borderColor);
+     * @fn void setBorderColor(const RGBAcolor& borderColor);
      */
-    void setBorderColor(nsGraphics::RGBAcolor& borderColor);
+    void setBorderColor(const nsGraphics::RGBAcolor& borderColor);
 
 protected:
     /**

src/shape/shape.cpp
@@ -20,7 +20,17 @@ const nsGraphics::RGBAcolor &nsShape::Shape::getFillColor() const
     return m_fillColor;
 } // getFillColor()
 
+void nsShape::Shape::setFillColor(const nsGraphics::RGBAcolor &fillColor)
+{
+    m_fillColor = fillColor;
+} // setFillColor()
+
 const nsGraphics::RGBAcolor &nsShape::Shape::getBorderColor() const
 {
     return m_borderColor;
 } // getBorderColor()
+
+void nsShape::Shape::setBorderColor(const nsGraphics::RGBAcolor &borderColor)
+{
+    m_borderColor = borderColor;
+} // setBorderColor()